2c06786b0df7b46ca27b8fa63f4fbafa6aa3195c
Veniamin Polienko <kasaklalita@mail.ru>
2022-11-03 20:44:48 +0300

    Coroutine context

--- LearningCoroutines/app/src/main/java/com/kasaklalita/learningcoroutines/MainActivity.kt
+++ LearningCoroutines/app/src/main/java/com/kasaklalita/learningcoroutines/MainActivity.kt
@@ -3,30 +3,34 @@ package com.kasaklalita.learningcoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            val networkCallAnswer = doNetworkCall()
-            val networkCallAnswer2 = doNetworkCall2()
-            Log.d("MainActivity", networkCallAnswer)
-            Log.d("MainActivity", networkCallAnswer2)
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG, "Starting corouting in thread ${Thread.currentThread().name}")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
+                tvDummy.text = answer
+            }
         }
-        Log.d("MainActivity", "hello from thread ${Thread.currentThread().name}")
     }
+}
 
-    suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "This is the answer"
-    }
-    suspend fun doNetworkCall2(): String {
-        delay(3000L)
-        return "This is the answer"
-    }
+suspend fun doNetworkCall(): String {
+    delay(3000L)
+    return "This is the answer"
+}
+
+suspend fun doNetworkCall2(): String {
+    delay(3000L)
+    return "This is the answer"
 }
